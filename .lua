8e41d4ea498ad46c3c07421567a77fa51bc1315b
Rynix <driftwynn2008@gmail.com>
2025-12-09 05:15:08 +0800

    Create .lua

---
+++ .lua
@@ -0,0 +1,8 @@
+If game PlaceId = 125723653259639
+then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Driftwyn/RynixHub2/refs/heads/main/.lua"))()
+end
+else
+if game PlaceId = 132128062320193
+  then
+  
