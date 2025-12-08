417897020c1c1f96cbacb7aa3877645d1d95f178
Rynix <driftwynn2008@gmail.com>
2025-12-09 05:17:17 +0800

    Update .lua

--- .lua
+++ .lua
@@ -1,8 +1,5 @@
-If game PlaceId = 125723653259639
-then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Driftwyn/RynixHub2/refs/heads/main/.lua"))()
+if game.PlaceId == 125723653259639 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Driftwyn/RynixHub2/refs/heads/main/.lua"))()
+elseif game.PlaceId == 132128062320193 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Driftwyn/RynixHub/refs/heads/main/.lua"))()
 end
-else
-if game PlaceId = 132128062320193
-  then
-  
