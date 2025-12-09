a5af7909ba1c034e4e0c2051ab2fde2b0f0a3aa2
Rynix <driftwynn2008@gmail.com>
2025-12-09 09:11:40 +0800

    Update .lua

--- .lua
+++ .lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 125723653259639 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Driftwyn/RynixHub2/refs/heads/main/.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Driftwyn/Drill-Digging-Simulator/refs/heads/main/.lua"))()
 elseif game.PlaceId == 132128062320193 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Driftwyn/RynixHub/refs/heads/main/.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Driftwyn/Gone-Hunting/refs/heads/main/.lua"))()
 end
